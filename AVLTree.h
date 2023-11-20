0689f7c767ead0fd74db50e98d5cbe74cac5e794
richardnguyen0715 <117381750+richardnguyen0715@users.noreply.github.com>
2023-11-21 06:36:22 +0700

    AVL.h

--- AVLTree.h
+++ AVLTree.h
@@ -1,475 +1,346 @@
 #pragma once
 #include<iostream>
-#include<math.h>
-#include"stack.h"
 #include"queue.h"
+#include"stack.h"
 
-namespace AVLTree {
+template<typename T>
+struct avlNode {
+	T data;
+	avlNode* left;
+	avlNode* right;
+	int height;
+};
 
-	template<typename T>
-	struct Node {
-		T key;
-		Node* left;
-		Node* right;
-		int height;
-	};
+template<typename T>
+avlNode<T>* makeAvlNode(T key) {
+	avlNode<T>* newNode = new avlNode<T>;
+	newNode->data = key;
+	newNode->left = nullptr;
+	newNode->right = nullptr;
+	newNode->height = 0;
+	return newNode;
+}
 
-	template<typename T>
-	struct Tree {
+template<typename T>
+struct Avl {
+	avlNode<T>* root;
 
-		Node<T>* root;
+	void init() {
+		root = nullptr;
+	}
 
-		//initialize a binary search tree
-		void init() {
-			root == nullptr;
-		}
+	void deinit(avlNode<T>* root) {
+		if (root == nullptr) return;
+		deinit(root->left);
+		deinit(root->right);
+		delete root;
+		root = nullptr;
+	}
 
-		//deinitialize a binary search tree
-		void deinit(Node<T>*& root) {
-			if (root == nullptr) return;
-			deinit(root->left);
-			deinit(root->right);
-			delete root;
-		}
+	void deinit() {
+		deinit(root);
+	}
 
-		//is the tree empty?
-		bool empty() {
-			if (root == nullptr) return true;
-			return false;
-		}
+	bool empty() {
+		if (root == nullptr) return true;
+		return false;
+	}
 
-		//create a new node
-		Node<T>* creatNode(T data) {
-			Node<T>* temp = new Node<T>;
-			temp->key = data;
-			temp->left = nullptr;
-			temp->right = nullptr;
-			temp->height = 0;
-			return temp;
-		}
+	// Preoder travesel
+	void NLR(avlNode<T>* root) {
+		if (root == nullptr) return;
+		cout << root->data << " ";
+		NLR(root->left);
+		NLR(root->right);
+	}
 
-		//  PreOder Travesel - NLR
-		void NLR(Node<T>* root) {
-			if (root == nullptr) return;
-			cout << root->key << " ";
-			NLR(root->left);
-			NLR(root->right);
-		}
+	// Overloading
+	void NLR() {
+		NLR(root);
+	}
 
-		// inOder travesel - LNR
-		void LNR(Node<T>* root) {
-			if (root == nullptr) return;
-			LNR(root->left);
-			cout << root->key << " ";
-			LNR(root->right);
-		}
+	// Inoder travesel
+	void LNR(avlNode<T>* root) {
+		if (root == nullptr) return;
+		LNR(root->left);
+		cout << root->data << " ";
+		LNR(root->right);
+	}
 
-		// posOder travesel - LRN
-		void LRN(Node<T>* root) {
-			if (root == nullptr) return;
-			LRN(root->left);
-			LRN(root->right);
-			cout << root->key << " ";
-		}
+	// Overloading
+	void LNR() {
+		LNR(root);
+	}
 
-		// levelOder travesel 
-		void levelOder(Node<T>* root) {
-			if (root == nullptr) {
-				cout << "Tree is empty! \n";
-				return;
+	// Posoder travesel
+	void LRN(avlNode<T>* root) {
+		if (root == nullptr) return;
+		LRN(root->left);
+		LRN(root->right);
+		cout << root->data << " ";
+	}
+
+	// Overloading
+	void LRN() {
+		LRN(root);
+	}
+
+	// Leveloder travesel
+	void levelOder(avlNode<T>* root) {
+		if (root == nullptr) return;
+		queue<avlNode<T>* > q;
+		q.init();
+		q.push(root);
+		q.push(NULL);
+		avlNode<T>* newNode = root;
+		while (q.size() > 1) {
+			newNode = q.front->data;
+			q.pop();
+			if (newNode == NULL) {
+				cout << endl;
+				q.push(NULL);
 			}
-			functionQueue::queue<Node<T>*> q;
-			q.init();
-			q.push(root);
-			q.push(NULL);
-			Node<T>* temp = NULL;
-			while (q.size() > 1) {
-				temp = q.front->key;
-				q.pop();
-				if (temp == NULL) { // Finished a level
-					q.push(NULL);
-					cout << endl;
+			else {
+				if (newNode->left != nullptr) {
+					q.push(newNode->left);
 				}
-				else {
-					if (temp->left != nullptr) {
-						q.push(temp->left);
-					}
-					if (temp->right != nullptr) {
-						q.push(temp->right);
-					}
-					cout << temp->key << " ";
+				if (newNode->right != nullptr) {
+					q.push(newNode->right);
 				}
+				cout << newNode->data << " ";
 			}
 		}
+	}
 
-		//Seach a key in tree
-		Node<T>* search(Node<T>* root, T key) {
-			if (root == nullptr) {
-				cout<< "Key is not exist";
-				return nullptr;
-			}
-			if (key < root->key) {
-				search(root->left, key);
-			}
-			else if (key > root->key) {
-				search(root->right, key);
-			}
-			else if (key == root->key) {
-				return root;
-			}
-		}
+	// Overloading
+	void levelOder() {
+		levelOder(root);
+	}
 
-		//Rotate left
-		Node<T>* rotateLeft(Node<T>* root) {
-			Node<T>* rightChild = root->right;
-			Node<T>* leftOfRightChild = rightChild->left;
-			root->right = leftOfRightChild;
-			rightChild->left = root;
-			return rightChild;
+	// Print 2D Tree in a pretty way
+	void print2DTree(avlNode<T>* root, int space) {
+		if (root == nullptr) return;
+		space += 10;
+		print2DTree(root->right, space);
+		cout << endl;
+		for (int i = 10; i < space; i++) {
+			cout << " ";
 		}
+		cout << root->data << endl;
+		print2DTree(root->left, space);
+	}
 
-		//Rotate right
-		Node<T>* rotateRight(Node<T>* root) {
-			Node<T>* leftChild = root->left;
-			Node<T>* rightOfLeftChild = leftChild->right;
-			root->left = rightOfLeftChild;
-			leftChild->right = root;
-			return leftChild;
-		}
+	// Overloading
+	void print2DTree() {
+		print2DTree(root, 0);
+	}
 
-		//Check sitiuation of a unbalance node
-		int checkSitiuation(Node<T>* root) {
-			if (root != nullptr) {
-				if (height(root->left) - height(root->right) > 0) { // Left deviated
-					if (height(root->left->left) - height(root->left->right) > 0)
-						return 1;	//Left - Left
-					else	
-						return 2;	// Left - Right
-				}
-				else { // Right deviated
-					if (height(root->right->left) - height(root->right->right) > 0) 
-						return 3;	// Right - Left
-					else    
-						return 4;	// Right - Right
-				}
-			}
+	// Push a node into tree
+	void push(avlNode<T>*& root, T key) {
+		if (root == nullptr) {
+			avlNode<T>* newNode = makeAvlNode(key);
+			root = newNode;
 		}
+		else if (key < root->data) {
+			push(root->left, key);
+		}
+		else if (key > root->data) {
+			push(root->right, key);
+		}
+		else if (key == root->data) {
+			;
+		}
+	}
 
-		//Is node balance?
-		bool isBalance(Node<T>* root) {
-			if (root != nullptr) {
-				if (abs(height(root->left) - height(root->right)) > 1) {
-					return false;
-				}
-			}
-			return true;
-		}
+	// Overloading 
+	void push(T key) {
+		push(root, key);
+		balanceTree(root);
+		updateHeight(root);
+	}
 
-		Node<T>* findParent(T x, Node<T>* root) {
-			Node<T>* temp = root;
-			while (temp != nullptr) {
-				if (x < temp->key) {
-					if (temp->left != nullptr) {
-						if (temp->left->key == x) {
-							return temp;
-						}
-						else {
-							temp = temp->left;
-						}
-					}
-					else {
-						cout << "Node x khong ton tai";
-						return nullptr;
-					}
-				}
-				else if (x > temp->key) {
-					if (temp->right != nullptr) {
-						if (temp->right->key == x) {
-							return temp;
-						}
-						else {
-							temp = temp->right;
-						}
-					}
-					else {
-						cout << "Node x khong ton tai";
-						return nullptr;
-					}
-				}
-				else {
-					return temp;
-				}
-			}
+	// Find minimum of right child tree
+	void minimum(avlNode<T>*& root, avlNode<T>*& temp) {
+		if (root->left == nullptr) {
+			temp->data = root->data;
+			temp = root;
+			root = root->right;
 		}
+		else {
+			minimum(root->left, temp);
+		}
+	}
 
-		//Rotate node to right if it not balance 
-		Node<T>* rotateNode(Node<T>* &root) {
-			if (root == nullptr) {	
-				throw "Out of range!";
-				return nullptr;
-			}
-			else if (checkSitiuation(root) == 1) { // Left - Left
-				root = rotateRight(root);
-			}
-			else if (checkSitiuation(root) == 4) { // Right - Right
-				root = rotateLeft(root);
-			}
-			else if (checkSitiuation(root) == 2) { // Left - Right
-				root->left = rotateLeft(root->left);
-				root = rotateRight(root);
-			}
-			else if (checkSitiuation(root) == 3) { // Right - Left
-				root->right = rotateRight(root->right);
-				root = rotateLeft(root);
-			}
-			return root;
+	// Remove a node out of tree
+	void remove(avlNode<T>*& root, T key) {
+		if (key < root->data) {
+			remove(root->left, key);
 		}
-
-		//Balance a tree
-		void balanceTree(Node<T>* &run, Node<T>* root ) {
-			if (run == nullptr) return;
-			balanceTree(run->left, root);
-			balanceTree(run->right, root);
-			if (isBalance(run) == false) {
-				Node<T>* parent = findParent(run->key, root);
-				if (run->key > parent->key) {
-					parent->right = rotateNode(run);
-				}
-				else if (run->key < parent->key)  {
-					parent->left = rotateNode(run);
-				}
-				else if (run->key == parent->key) {
-					run = rotateNode(run);
-				}
-			}
+		else if (key > root->data) {
+			remove(root->right, key);
 		}
-
-		//Update Height of Node
-		void updateHeight(Node<T>* root) {
-			if (root == nullptr) return;
-			root->height = height(root);
-			updateHeight(root->left);
-			updateHeight(root->right);
-		}
-		
-		//Insert a node into a tree
-		bool insertNode(Node<T>*& root, T key) {
-			if (root == nullptr) {
-				Node<T>* temp = creatNode(key);
-				if (temp == nullptr) {
-					throw "can not creat node! \n";
-					return false;
-				}
-				root = temp;
-				return true;
-			}
-			else if (key < root->key) {
-				insert(root->left, key);
-			}
-			else if (key > root->key) {
-				insert(root->right, key);
-			}
-			else if (key == root->key) {
-				return true;
-			}
-		}
-
-		void insert(Node<T>* &root, T key) {
-			insertNode(root, key);
-			balanceTree(root, root);
-			updateHeight(root);
-		}
-
-		//Find the minimist node of right child 
-		void minimum(Node<T>*& root, Node<T>*& mini) {
+		else {
+			avlNode<T>* temp = root;
 			if (root->left == nullptr) {
-				mini->key = root->key;
-				mini = root;
 				root = root->right;
 			}
+			else if (root->right == nullptr) {
+				root = root->left;
+			}
 			else {
-				minimum(root->left, mini);
+				minimum(root, temp);
 			}
+			delete temp;
+			temp = nullptr;
 		}
+	}
 
-		//Remove a node in tree
-		bool removeNode(Node<T>*& root, T data) {
-			if (root == nullptr) {
-				throw "Data is not exist! \n";
-				return false;
-			}
-			else if (data < root->key) {
-				remove(root->left, data);
-			}
-			else if (data > root->key) {
-				remove(root->right, data);
-			}
-			else if (data == root->key) {
-				Node<T>* mini = root;
-				if (root->left == nullptr) {
-					root = root->right;
-				}
-				else if (root->right == nullptr) {
-					root = root->left;
-				}
-				else {
-					minimum(root->right, mini);
-				}
-				delete mini;
-			}
-			return true;
+	// Overloading
+	void remove(T key) {
+		remove(root, key);
+		balanceTree(root);
+		updateHeight(root);
+	}
+
+	// search a node in tree
+	avlNode<T>* search(avlNode<T>* root, T key) {
+		if (root == nullptr) {
+			return nullptr;
 		}
-
-		//Remove node and balance 
-		void remove(Node<T>*& root, T data) {
-			removeNode(root, data);
-			balanceTree(root, root);
-			updateHeight(root);
+		else if (key < root->data) {
+			search(root->left, key);
 		}
-
-		//Create tree from an array
-		Node<T>* arrayToTree(T a[], int n) {
-			Node<T>* temp = nullptr;
-			for (int i = 0; i < n; i++) {
-				insert(temp, a[i]);
-			}
-			return temp;
+		else if (key > root->data) {
+			search(root->right, key);
 		}
-
-		//Height of node
-		int height(Node<T>* root) {
-			if (root == nullptr) return 0;
-			return 1 + max(height(root->left), height(root->right));
+		else {
+			return root;
 		}
+	}
 
-		//Create an array from a tree
-		void treeToArray(T a[], int& n, Node<T>* root) {
-			if (root == nullptr) return;
-			treeToArray(a, n, root->left);
-			a[n] = root->key;
-			n++;
-			treeToArray(a, n, root->right);
+	// Overloading
+	avlNode<T>* search(T key) {
+		return search(root, key);
+	}
+
+	// Find parent of a node in tree
+	avlNode<T>* findParent(avlNode<T>* root, T key) {
+		if (root == nullptr) return nullptr;
+		if (root->data == key) {
+			return root;
 		}
-
-		//Count the number of nodes in a tree
-		int countNode(Node<T>* root) {
-			int n = 0;
-			T a[30];
-			treeToArray(a, n, root);
-			return n;
+		else if (key < root->data) {
+			if (root->left->data == key) return root;
+			findParent(root->left, key);
 		}
-
-		//Return sum of nodes in a tree
-		int sumNode(Node<T>* root) {
-			int n = 0;
-			int sum = 0;
-			T a[30];
-			treeToArray(a, n, root);
-			for (int i = 0; i < n; i++) {
-				sum += a[i];
-			}
-			return sum;
+		else if (key > root->data) {
+			if (root->right->data == key) return root;
+			findParent(root->right, key);
 		}
+	}
 
-		//Height of node has a default key
-		int heightKey(Node<T>* root, T key) {
-			Node<T>* temp = search(root, key);
-			if (temp == nullptr) {
-				throw "Key is not exist!\n";
-				return 0;
-			}
-			return height(temp);
+	// Overloading
+	avlNode<T>* findParent(T key) {
+		return findParent(root, key);
+	}
+
+	// Find height of Node
+	int height(avlNode<T>* root) {
+		if (root == nullptr) return 0;
+		return 1 + max(height(root->left), height(root->right));
+	}
+
+	// Convert a tree into an array
+	void treeToArray(T a[], int& arraySize, avlNode<T>* root) {
+		treeToArray(a, arraySize, root->left);
+		a[arraySize] = root->data;
+		arraySize++;
+		treeToArray(a, arraySize, root->right);
+	}
+
+	// Overloading
+	void treeToArray(T a[], int& arraySize) {
+		treeToArray(a, arraySize, root);
+	}
+
+	avlNode<T>* rotateLeft(avlNode<T>* root) {
+		avlNode<T>* rightChild = root->right;
+		avlNode<T>* leftRightChild = rightChild->left;
+		rightChild->left = root;
+		root->right = leftRightChild;
+		return rightChild;
+	}
+
+	avlNode<T>* rotateRight(avlNode<T>* root) {
+		avlNode<T>* leftChild = root->left;
+		avlNode<T>* rightLeftChild = leftChild->right;
+		leftChild->right = root;
+		root->left = rightLeftChild;
+		return leftChild;
+	}
+
+	int checkSitiuation(avlNode<T>* root) {
+		if (root == nullptr) return 0;
+		if (height(root->left) - height(root->right) > 0) { // Left Deviated
+			if (height(root->left->left) - height(root->left->right) > 0) return 1; // L - L
+			return 2; // L - R
 		}
-
-		//Level of a node in tree
-		int levelOfNode(Node<T>* root, Node<T>* p) {
-			return height(root) - height(p);
+		else { // Right Deviated
+			if (height(root->right->right) - height(root->right->left) > 0) return 3; // R - R
+			return 4; // R - L
 		}
+	}
 
-		//Count the number of node leaf in a tree
-		int countLeaf(Node<T>* root) {
-			if (root == nullptr) return 0;
-			if (root->left == nullptr && root->right == nullptr) return 1;
-			return countLeaf(root->left) + countLeaf(root->right);
+	void rotateNode(avlNode<T>* &root) {
+		if (root == nullptr) return;
+		if (checkSitiuation(root) == 1) {
+			root = rotateRight(root);
 		}
-
-		//Count the number of node which is less than a key in a tree
-		int countLess(Node<T>* root, T key) {
-			int n = 0;
-			int cnt = 0;
-			T a[30];
-			treeToArray(a, n, root);
-			for (int i = 0; i < n; i++) {
-				if (key > a[i]) {
-					cnt++;
-				}
-			}
-			return cnt;
+		else if (checkSitiuation(root) == 3) {
+			root = rotateLeft(root);
 		}
-
-		//Count the number of node which is greater than a key in a tree
-		int countGreat(Node<T>* root, T key) {
-			int n = 0;
-			int cnt = 0;
-			T a[30];
-			treeToArray(a, n, root);
-			for (int i = 0; i < n; i++) {
-				if (key < a[i]) {
-					cnt++;
-				}
-			}
-			return cnt;
+		else if (checkSitiuation(root) == 2) {
+			root->left = rotateLeft(root->left);
+			root = rotateRight(root);
 		}
-
-		//Is the tree a binary search tree?
-		bool isBST(Node<T>* root) {
-			int n = 0;
-			T a[30];
-			treeToArray(a, n, root);
-			for (int i = 0; i < n - 1; i++) {
-				if (a[i] >= a[i + 1]) return false;
-			}
-			return true;
+		else if (checkSitiuation(root) == 4) {
+			root->right = rotateRight(root->right);
+			root = rotateLeft(root);
 		}
+	}
 
-		// Is the tree a AVL tree?
-		bool isAVL(Node<T>* root) {
-			if (root == nullptr) return true;
-			if (isBalance(root) == true) return true;
-			else {
-				return false;
-			}
-			return isAVL(root->left) && isAVL(root->right);
+	void balanceTree(avlNode<T>* &root) {
+		if (root == nullptr) return;
+		balanceTree(root->left);
+		balanceTree(root->right);
+		if (isBalance(root) == false) {
+			rotateNode(root);
 		}
+	}
 
-		//Show heiht of node
-		void showHeight(Node<T>* root) {
-			functionQueue::queue<Node<T>*> q;
-			q.init();
-			q.push(root);
-			q.push(NULL);
-			Node<T>* temp = nullptr;
-			while (q.size() > 1) {
-				temp = q.front->key;
-				q.pop();
-				if (temp == NULL) {
-					q.push(NULL);
-					cout << endl;
-				}
-				else {
-					if (temp->left != NULL) {
-						q.push(temp->left);
-					}
-					if (temp->right != NULL) {
-						q.push(temp->right);
-					}
-					cout << temp->height << " ";
-				}
-			}
-		}
+	void updateHeight(avlNode<T>* &root) {
+		if (root == nullptr) return;
+		updateHeight(root->left);
+		updateHeight(root->right);
+		root->height = height(root);
+	}
 
-	//Struct Tree
-	};
+	bool isBalance(avlNode<T>* root) {
+		if (root == nullptr) return true; 
+		if (abs(height(root->left) - height(root->right)) > 1) return false;
+		return true;
+	}
 
-//Namespace
-}
+	// Is this tree balance?
+	bool isAVLTree(avlNode<T>* root) {
+		if (root == nullptr) return true;
+		if (abs(height(root->left) - height(root->right)) > 1) return false;
+		return isAVLTree(root->left) && isAVLTree(root->right);
+	}
+
+	// Overloading
+	void isAVLTree() {
+		isAVLTree(root);
+	}
+
+};
